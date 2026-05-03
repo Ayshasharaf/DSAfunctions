cfa66e24285b192c1ecf3300cf914abd8b476729
Ayshasharaf <1221303018@student.mmu.edu.my>
2026-05-03 10:27:50 +0400

    more functions

--- ArrayUtils.java
+++ ArrayUtils.java
@@ -17,8 +17,14 @@ public class ArrayUtils {
         while (true) {
         System.out.println("\nMENU");
         System.out.println("1. Find Max");
-        //System.out.println("2. Reverse");
-        System.out.println("2. Exit");
+        System.out.println("2. Reverse");
+        System.out.println("3. Find Min");
+        System.out.println("4. Find Average");
+        System.out.println("5. Find Sum");
+        System.out.println("6. Find Occurrence");
+        System.out.println("7. Check Order");
+        System.out.println("8. Exit");
+
 
         int choice = sc.nextInt();
 
@@ -27,13 +33,37 @@ public class ArrayUtils {
                 System.out.println("Max: " + Functions.findMax(arr));
                 break;
 
-            /**case 2:
-                reverse(arr);
+            case 2:
+                Functions.reverse(arr);
                 System.out.println("Reversed: " + arr);
                 break;
-                **/
+                
 
-            case 2:
+            case 3:
+                System.out.println("Min: " + Functions.findMin(arr));
+                break;
+
+            case 4:
+                System.out.println("Average: " + Functions.findAverage(arr));
+                break;
+
+            case 5:
+                System.out.println("Sum: " + Functions.findSum(arr));
+                break;
+
+            case 6:
+                System.out.println("Enter the target element:");
+                int target = sc.nextInt();
+                System.out.println("Occurrence: " + Functions.findOccurence(arr, target));
+                break;
+
+            case 7:
+                System.out.println("Order: " + Functions.checkOrder(arr));
+                break;
+
+            case 8:
+                System.out.println("Exiting...");
+                sc.close();
                 return;
 
             default:
--- Functions.java
+++ Functions.java
@@ -10,4 +10,99 @@ public class Functions {
         }
         return max;     
     }
+    public static void reverse(ArrayList<Integer> arr){
+       int left =0;
+       int right = arr.size()-1;
+       while(left < right){
+        int temp = arr.get(left);
+        arr.set(left, arr.get(right));
+        arr.set(right, temp);
+        left++;
+        right--;
+       }
+    }
+    public static int findMin(ArrayList<Integer> arr){
+        int min = arr.get(0);
+        for(int i =1; i<arr.size(); i++){
+            if(arr.get(i)<min){
+                min=arr.get(i);
+            }
+        }
+        return min;
+    }
+    public static int findSum(ArrayList<Integer> arr){
+        int sum=0;
+        for (int i=0; i < arr.size(); i++){
+            sum+=arr.get(i);
+        }
+        return sum;
+    }
+    public static int findAverage(ArrayList<Integer> arr){
+        int sum= findSum(arr);
+        return sum/arr.size();
+    }
+    public static int findOccurence(ArrayList<  Integer> arr , int target){
+        int occurence=0;
+        for (int i =0; i<arr.size(); i++){
+            if (arr.get(i)==target){
+                occurence++;
+            } 
+        }
+        return occurence;
+
+    }
+    public static int findSecondLargest(ArrayList<Integer> arr){
+        int max = Integer.MIN_VALUE;
+        int secondMax=Integer.MIN_VALUE;
+        for (int i = 0 ; i < arr.size(); i++){
+            if (arr.get(i) > max){
+                secondMax = max;
+                max = arr.get(i);
+            } else if (arr.get(i) > secondMax && arr.get(i) != max){
+                secondMax = arr.get(i);
+            }
+        }
+        return secondMax;
+    }
+    public static ArrayList<Integer> removeDuplicates(ArrayList<Integer> arr){
+        HashSet<Integer> uniqueArr = new HashSet<>();
+        for (int num : arr) {
+            uniqueArr.add(num);
+        }
+        return new ArrayList<>(uniqueArr);
+    }
+    public static String isPalindrom(ArrayList<Integer> arr){
+        int left=0;
+        int right=arr.size()-1;
+        while(left<right){
+            if (arr.get(left)!=arr.get(right)){
+                return "Not Palindrome";
+            }
+            left++;
+            right--;    
+        }
+        return "Palindrome";
+    }
+    public static String checkOrder(ArrayList<Integer> arr) {
+        boolean isAscending = true;
+        boolean isDescending = true;
+
+        for (int i = 1; i < arr.size(); i++) {
+            if (arr.get(i) > arr.get(i - 1)) {
+                isDescending = false;
+            } else if (arr.get(i) < arr.get(i - 1)) {
+                isAscending = false;
+            }
+        }
+
+        if (isAscending) {
+            return "Ascending";
+        } else if (isDescending) {
+            return "Descending";
+        } else {
+            return "Unordered";
+        }
+    }
+    
+
 }
